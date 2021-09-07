f5107c49c89b261e810eecedb8c223b1a18ca0b8
isobel <isobel@thoughtmachine.net>
2021-09-07 17:40:09 +0100

    Adding sleep to fake tests

--- .circleci/tests.py
+++ .circleci/tests.py
@@ -1,5 +1,7 @@
 from hello import app
+from time import sleep
 with app.test_client() as c:
     response = c.get('/') # Perhaps here is where we'd call a Woodhouse endpoint
     assert response.data == b'Hello World!'
+    sleep(240)
     assert response.status_code == 200
